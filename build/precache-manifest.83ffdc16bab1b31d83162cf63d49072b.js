b86a85235df4739c3730f4925693bdcb18370ffe
Emmett Brady <emmett.brady@gmail.com>
2020-06-23 14:18:55 -0700

    adding build folder

---
+++ build/precache-manifest.83ffdc16bab1b31d83162cf63d49072b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdaea21c767af66283da71b16dcba3ec",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "d86bfb22306f5a68e053",
+    "url": "/reactPortfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5c0b42d5df90d604423f",
+    "url": "/reactPortfolio/static/js/2.56adf9a1.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/reactPortfolio/static/js/2.56adf9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86bfb22306f5a68e053",
+    "url": "/reactPortfolio/static/js/main.d2d6f208.chunk.js"
+  },
+  {
+    "revision": "9b0f8995f6f76a603e43",
+    "url": "/reactPortfolio/static/js/runtime-main.2ba24d27.js"
+  }
+]);
