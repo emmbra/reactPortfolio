6b762d822a1ae0911cd20609c39229a6b7c2c7ac
Emmett Brady <emmett.brady@gmail.com>
2020-06-23 14:38:56 -0700

    Updates

---
+++ precache-manifest.32c50af1e69a1b774ad182a8f6a0a1fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c81b3beb61e0917a8ee67a3b099f47",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4394ffc30318f616ecfe",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5c0b42d5df90d604423f",
+    "url": "/portfolio/static/js/2.56adf9a1.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.56adf9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4394ffc30318f616ecfe",
+    "url": "/portfolio/static/js/main.02eca9fb.chunk.js"
+  },
+  {
+    "revision": "9a67a413ad95425131de",
+    "url": "/portfolio/static/js/runtime-main.3dd4bd1f.js"
+  }
+]);
