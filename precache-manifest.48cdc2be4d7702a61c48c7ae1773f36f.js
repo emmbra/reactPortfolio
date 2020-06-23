2316af675fb88f0d6954c6266888e149b9867ae6
Emmett Brady <emmett.brady@gmail.com>
2020-06-23 11:28:57 -0700

    Updates

---
+++ precache-manifest.48cdc2be4d7702a61c48c7ae1773f36f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c6d337cf6d7be35b65090220ebbeed",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "8a6bfca83f459d9c6794",
+    "url": "/reactPortfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5c0b42d5df90d604423f",
+    "url": "/reactPortfolio/static/js/2.56adf9a1.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/reactPortfolio/static/js/2.56adf9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a6bfca83f459d9c6794",
+    "url": "/reactPortfolio/static/js/main.a571bf42.chunk.js"
+  },
+  {
+    "revision": "9b0f8995f6f76a603e43",
+    "url": "/reactPortfolio/static/js/runtime-main.2ba24d27.js"
+  }
+]);
